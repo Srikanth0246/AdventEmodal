aa08b04b6ecdb043b09e5e368908c401921ea78c
Srikanth0246 <tippasanisrikanth9966@gmail.com>
2024-09-16 14:13:54 +0530

    commit

--- obj/Debug/net8.0/AdventEmodal.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventEmodal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventEmodal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b1d0da86e72b79c8cbb16c68399899eeb490fb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventEmodal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventEmodal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
